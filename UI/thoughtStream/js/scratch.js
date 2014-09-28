ef9814c664a17097d98e1ffe4dd83850553bbe50
Matt Sevrens <msevrens@gmail.com>
2014-09-27 19:08:42 -0700

    new script

---
+++ UI/thoughtStream/js/scratch.js
@@ -0,0 +1,68 @@
+/* Take a CSV and turn it into a
+thought stream visualization*/
+
+function thoughtStream(data) {
+
+	var words = Object.keys(data[0]),
+		totals = {},
+		select = multiSelect(data, words);
+
+	var max = d3.max(data, function(row) { return d3.max(d3.values(row))}) 
+
+	console.log(max)
+
+}
+
+/* Produces the SVG components that 
+make up the thought stream */
+
+function chart(data) {
+
+}
+
+/* Prepare a set of words for
+visualization */
+
+function formatData(data, selected) {
+
+}
+
+/* Creates a multiple selection widget
+for selection of ideas to display */
+
+function multiSelect(data, words) {
+
+	// Create Multiselect
+	var widget = d3.select("body").append("div").classed("widget", true),
+		select = widget.append("select").classed("multiselect", true),
+		params = {
+			"multiple" : "multiple",
+	 		"data-placeholder" : "Add thoughts"
+		}
+
+	// Create Basic Markup
+	select.attr(params)
+		.selectAll("option")
+		.data(words)
+		.enter()
+		.append("option")
+		.text(function(d){return d});
+
+	// Construct Widget
+	$('.multiselect').multiselect({
+		"enableFiltering" : true,
+		"includeSelectAllOption" : true,
+		"maxHeight" : 250
+    });
+
+    return select
+
+}
+
+$(document).ready(function() {
+    
+    var csvpath = "../../data/output/collective_stream.csv";
+	
+	d3.csv(csvpath, thoughtStream);
+
+});
