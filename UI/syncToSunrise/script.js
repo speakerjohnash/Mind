70664f84dbc8f1596f6065a068a2d0ae85a610bb
Matt Sevrens <msevrens@gmail.com>
2015-01-08 17:57:54 -0800

    got arc to draw based on inputs

--- UI/syncToSunrise/script.js
+++ UI/syncToSunrise/script.js
@@ -1,5 +1,22 @@
 (function syncToSunrise() {
-  
+
+  // Create Input Controls
+
+  var wake = d3.select(".day-bookend").append("input")
+    .attr("type", "time")
+    .attr("class", "wake")[0][0];
+
+  var sleep = d3.select(".day-bookend").append("input")
+    .attr("type", "time")
+    .attr("class", "sleep")[0][0];
+
+  // Set Defaults
+
+  wake.value = "09:00:00";
+  sleep.value = "23:00:00";
+
+  // Draw Arc Clock
+
   var canvas = d3.select("body").append("svg")
     .attr("width", 500)
     .attr("height", 500);
@@ -21,7 +38,29 @@
     .startAngle(0)
     .endAngle(time2Radians(moment()._d));
 
+  var wakeParts = wake.value.split(":"),
+      sleepParts = sleep.value.split(":"),
+      wakeAngle = time2Radians(moment().startOf('day').hour(wakeParts[0]).minute(wakeParts[1])._d),
+      sleepAngle = time2Radians(moment().startOf('day').hour(sleepParts[0]).minute(sleepParts[1])._d),
+      dayLength = sleepAngle - wakeAngle,
+      wakeAngleCentered = -(dayLength / 2),
+      sleepAngleCentered = wakeAngleCentered + dayLength;
+
+  console.log(wakeAngle)
+  console.log(sleepAngle)
+  console.log(wakeAngleCentered)
+  console.log(sleepAngleCentered)
+
+  var arc2 = d3.svg.arc()
+    .innerRadius((r / 2) - 5)
+    .outerRadius(r / 2)
+    .startAngle(wakeAngleCentered)
+    .endAngle(sleepAngleCentered);
+
   group.append("path")
     .attr("d", arc)
+
+  group.append("path")
+    .attr("d", arc2)
 
 })();
