947f3bb2098cbf37e5676cfd29a0ecab88c63d2b
msevrens <msevrens@gmail.com>
2015-01-14 20:17:02 -0800

    making work

--- mind/cluster_words.py
+++ mind/cluster_words.py
@@ -37,22 +37,28 @@ def cluster_vectors(word2vec):
 	"""Clusters a set of word vectors"""
 
 	n_clusters = int(word2vec.syn0.shape[0] / 20)
-	clusters = kmeans(n_clusters=500, max_iter=100, batch_size=200, n_init=1, init_size=2000)
+	clusters = kmeans(n_clusters=n_clusters, max_iter=100, batch_size=200, n_init=10, init_size=20)
 	tokens = word2vec.vocab.keys()
+	tokens = ["prophet", "thought", "mind", "truth", "thoughts", "time", "good", "meaning", "like", "future", "make", "context", "words", "self", "consciousness", "stream", "need", "just", "knowledge", "new", "today", "way", "does", "know", "people", "reality", "day", "idea", "life", "ideas", "use", "predictions", "conscious", "light", "word", "state", "things", "reflect", "information", "brain", "learning", "prediction", "better", "think", "past", "speak", "work", "right", "flow", "change", "learn", "meds", "http", "reflection", "feel", "mimi", "data", "true", "want", "human", "language", "best", "com", "truths", "real", "love", "means", "minds", "tomorrow", "world", "important", "tree", "users", "come", "really", "point", "search", "great", "site", "different", "predict", "private", "live", "tool", "path", "perception", "form", "concept", "meditation", "create", "able", "body", "seek", "needs", "used", "design", "flood", "place", "ken", "little", "waves", "subjective", "thinking", "thing", "sync", "set", "lot", "objective", "having", "rate", "present", "health", "using", "makes", "understand", "wisdom", "humans", "sunrise", "mental", "perceive", "ask", "far", "healthy", "current", "wave", "years", "religion", "feature", "long", "predictive", "energy", "post", "understanding", "build", "log", "permanence", "focus", "say", "cognitive", "contexts", "god", "lucid", "user", "lost", "machine", "year", "knowing", "neural", "useful", "questions", "end", "physical", "potential", "did", "old", "let", "clock", "control", "write", "rating", "link", "sense", "andy", "sun", "line", "button", "dream", "public", "usage", "challenging", "distributed", "book", "based", "going", "remember", "streams", "awareness", "root", "bound", "start", "share", "define", "semantic", "explore", "features", "statements", "question", "helps", "repeat", "reflections", "stretch"]
+	X = np.array([model[t].T for t in tokens])
 
-	X = np.array([word2vec[t].T for t in tokens])
-
+	clusters = kmeans(n_clusters=150, max_iter=100, batch_size=200, n_init=10, init_size=300)
 	clusters.fit(X)
 	word_clusters = {word:label for word, label in zip(tokens, clusters.labels_)}
 	sorted_clusters = sorted(word_clusters.items(), key=operator.itemgetter(1))
-	#safe_print(clusters.cluster_centers_)
+	collected = collections.defaultdict(list)
 
-	safe_print(sorted_clusters)
+	for k in sorted_clusters:
+		collected[k[1]].append(k[0])
+
+	for key in keys:
+		safe_print(key, collected[key], "\n")
+
 
 def run_from_command_line(command_line_arguments):
 	"""Runs the module when invoked from the command line."""
 
-	word2vec = Word2Vec.load(sys.argv[1])
+	word2vec = Word2Vec.load_word2vec_format(sys.argv[1], binary=True)
 	cluster_vectors(word2vec)
 
 if __name__ == "__main__":
