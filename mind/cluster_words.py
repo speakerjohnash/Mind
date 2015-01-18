5989ea3a91940e87cf510e87246a9cb32f1ad40a
Matt Sevrens <msevrens@gmail.com>
2015-01-18 13:59:25 -0800

    changing variable names so module will run

--- mind/cluster_words.py
+++ mind/cluster_words.py
@@ -16,6 +16,7 @@ Created on Dec 4, 2014
 import csv
 import sys
 import operator
+import collections
 
 import numpy as np
 from gensim.models import Word2Vec
@@ -37,13 +38,13 @@ def cluster_vectors(word2vec):
 	"""Clusters a set of word vectors"""
 
 	n_clusters = int(word2vec.syn0.shape[0] / 20)
-	tokens = word2vec.vocab.keys()
-	tokens = ["prophet", "thought", "mind", "truth", "thoughts", "time", "good", "meaning", "like", "future", "make", "context", "words", "self", "consciousness", "stream", "need", "just", "knowledge", "new", "today", "way", "does", "know", "people", "reality", "day", "idea", "life", "ideas", "use", "predictions", "conscious", "light", "word", "state", "things", "reflect", "information", "brain", "learning", "prediction", "better", "think", "past", "speak", "work", "right", "flow", "change", "learn", "meds", "http", "reflection", "feel", "mimi", "data", "true", "want", "human", "language", "best", "com", "truths", "real", "love", "means", "minds", "tomorrow", "world", "important", "tree", "users", "come", "really", "point", "search", "great", "site", "different", "predict", "private", "live", "tool", "path", "perception", "form", "concept", "meditation", "create", "able", "body", "seek", "needs", "used", "design", "flood", "place", "ken", "little", "waves", "subjective", "thinking", "thing", "sync", "set", "lot", "objective", "having", "rate", "present", "health", "using", "makes", "understand", "wisdom", "humans", "sunrise", "mental", "perceive", "ask", "far", "healthy", "current", "wave", "years", "religion", "feature", "long", "predictive", "energy", "post", "understanding", "build", "log", "permanence", "focus", "say", "cognitive", "contexts", "god", "lucid", "user", "lost", "machine", "year", "knowing", "neural", "useful", "questions", "end", "physical", "potential", "did", "old", "let", "clock", "control", "write", "rating", "link", "sense", "andy", "sun", "line", "button", "dream", "public", "usage", "challenging", "distributed", "book", "based", "going", "remember", "streams", "awareness", "root", "bound", "start", "share", "define", "semantic", "explore", "features", "statements", "question", "helps", "repeat", "reflections", "stretch"]
-	X = np.array([model[t].T for t in tokens])
+	keys = word2vec.vocab.keys()
+	keys = ["prophet", "thought", "mind", "truth", "thoughts", "time", "good", "meaning", "like", "future", "make", "context", "words", "self", "consciousness", "stream", "need", "just", "knowledge", "new", "today", "way", "does", "know", "people", "reality", "day", "idea", "life", "ideas", "use", "predictions", "conscious", "light", "word", "state", "things", "reflect", "information", "brain", "learning", "prediction", "better", "think", "past", "speak", "work", "right", "flow", "change", "learn", "meds", "reflection", "feel", "data", "true", "want", "human", "language", "best", "truths", "real", "love", "means", "minds", "tomorrow", "world", "important", "tree", "users", "come", "really", "point", "search", "great", "site", "different", "predict", "private", "live", "tool", "path", "perception", "form", "concept", "meditation", "create", "able", "body", "seek", "needs", "used", "design", "flood", "place", "ken", "little", "waves", "subjective", "thinking", "thing", "sync", "set", "lot", "objective", "having", "rate", "present", "health", "using", "makes", "understand", "wisdom", "humans", "sunrise", "mental", "perceive", "ask", "far", "healthy", "current", "wave", "years", "religion", "feature", "long", "predictive", "energy", "post", "understanding", "build", "log", "permanence", "focus", "say", "cognitive", "contexts", "god", "lucid", "user", "lost", "machine", "year", "knowing", "neural", "useful", "questions", "end", "physical", "potential", "did", "old", "let", "clock", "control", "write", "rating", "link", "sense", "sun", "line", "button", "dream", "public", "usage", "challenging", "distributed", "book", "based", "going", "remember", "streams", "awareness", "root", "bound", "start", "share", "define", "semantic", "explore", "features", "statements", "question", "helps", "reflections", "stretch"]
+	X = np.array([word2vec[t].T for t in keys])
 
-	clusters = kmeans(n_clusters=10, max_iter=100, batch_size=200, n_init=10, init_size=300)
+	clusters = kmeans(n_clusters=25, max_iter=100, batch_size=200, n_init=10, init_size=300)   
 	clusters.fit(X)
-	word_clusters = {word:label for word, label in zip(tokens, clusters.labels_)}
+	word_clusters = {word:label for word, label in zip(keys, clusters.labels_)}
 	sorted_clusters = sorted(word_clusters.items(), key=operator.itemgetter(1))
 	collected = collections.defaultdict(list)
 
@@ -52,7 +53,6 @@ def cluster_vectors(word2vec):
 
 	for key in keys:
 		safe_print(key, collected[key], "\n")
-
 
 def run_from_command_line(command_line_arguments):
 	"""Runs the module when invoked from the command line."""
