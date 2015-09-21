0d08ea7e519d185bc5c0c59b4820dc2d9fd7fdb5
Matt Sevrens <msevrens@gmail.com>
2015-09-21 15:46:24 -0700

    thoughts need to be batched somewhat

--- mind/apply_CNN.py
+++ mind/apply_CNN.py
@@ -2,6 +2,8 @@ import csv
 import sys
 import math
 
+from itertools import zip_longest
+
 import pandas as pd
 import numpy as np
 
@@ -14,10 +16,19 @@ from mind.load_CNN import get_CNN
 
 #####################################################
 
+def grouper(iterable):
+	return zip_longest(*[iter(iterable)]*1000, fillvalue={"description":""})
+
 CLASSIFIER = get_CNN(sys.argv[2])
 
-df = pd.read_csv(sys.argv[1], na_filter=False, quoting=csv.QUOTE_NONE, encoding="utf-8", sep="|", error_bad_lines=False)
-trans = list(df.T.to_dict().values())
-trans = CLASSIFIER(trans, doc_key="Thought", label_key=sys.argv[3])
-out_df = pd.DataFrame(trans)
+df = pd.read_csv(sys.argv[1], na_filter=False, encoding="utf-8", error_bad_lines=False)
+thoughts = list(df.T.to_dict().values())
+
+processed = []
+batches = grouper(thoughts)
+	
+for i, batch in enumerate(batches):
+	processed += CLASSIFIER(batch, doc_key="Thought", label_key=sys.argv[3])
+
+out_df = pd.DataFrame(processed)
 out_df.to_csv("data/output/classified_thought.csv", sep="|", mode="w", encoding="utf-8", index=False, index_label=False)
