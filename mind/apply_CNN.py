300cdc548a376105a74a01305113512e7ec1b823
Matt Sevrens <msevrens@gmail.com>
2015-09-21 16:29:44 -0700

    changes to batching

--- mind/apply_CNN.py
+++ mind/apply_CNN.py
@@ -17,7 +17,7 @@ from mind.load_CNN import get_CNN
 #####################################################
 
 def grouper(iterable):
-	return zip_longest(*[iter(iterable)]*1000, fillvalue={"description":""})
+	return zip_longest(*[iter(iterable)]*1000, fillvalue={"Thought":""})
 
 CLASSIFIER = get_CNN(sys.argv[2])
 
@@ -30,5 +30,6 @@ batches = grouper(thoughts)
 for i, batch in enumerate(batches):
 	processed += CLASSIFIER(batch, doc_key="Thought", label_key=sys.argv[3])
 
+processed = processed[0:len(thoughts)]
 out_df = pd.DataFrame(processed)
 out_df.to_csv("data/output/classified_thought.csv", sep="|", mode="w", encoding="utf-8", index=False, index_label=False)
