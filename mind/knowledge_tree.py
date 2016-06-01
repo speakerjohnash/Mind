87bf11c0bcd9c5b6b093667a1cf3701840695229
Matt Sevrens <msevrens@gmail.com>
2016-05-31 23:03:57 -0700

    adding fruiting code

--- mind/knowledge_tree.py
+++ mind/knowledge_tree.py
@@ -20,8 +20,10 @@ import math
 import json
 import operator
 
+import editdistance
 import pandas as pd
 
+import gensim
 from gensim.models import word2vec
 
 from mind.tools import dict_2_json, vectorize, word_count
@@ -29,20 +31,20 @@ from mind.tools import dict_2_json, vectorize, word_count
 # Load Model
 model_path = "models/word2vec.bin"
 print("Loading model...")
-model = word2vec.Word2Vec.load_word2vec_format(model_path, binary=True)  # C binary format
+model = gensim.models.Word2Vec.load('models/prophet_word2vec')  # C binary format
 print("Loading model: Done")
 
 # Load Top Words
 df = pd.read_csv("data/input/thought.csv", na_filter=False, encoding="utf-8", error_bad_lines=False)
 seers_grouped = df.groupby('Seer', as_index=False)
 seers = dict(list(seers_grouped))
-thoughts = list(seers[sys.argv[1]]["Thought"])
+thoughts = list(seers["prophet"]["Thought"]) + list(seers["msevrens@yodlee.com"]["Thought"]) + list(seers["msevrens"]["Thought"])
 ken = vectorize(thoughts, min_df=1)
 sorted_ken = sorted(ken.items(), key=operator.itemgetter(1))
 sorted_ken.reverse()
 
 keys_in_word2vec = model.vocab.keys()
-tokens = [x[0] for x in sorted_ken[0:2500] if x[0] in keys_in_word2vec]
+tokens = [x[0] for x in sorted_ken[0:5000] if x[0] in keys_in_word2vec]
 
 number_words = len(model.vocab)
 vocab = list(model.vocab.keys())
@@ -52,23 +54,35 @@ similarity_lookup = {}
 for i in range(0, number_words):
 	word = vocab[i]
 
-	if word not in tokens:
+	if word not in tokens or word.isdigit():
 		continue
 
 	nearest_words = model.most_similar(positive=[word], negative=[], topn=5000)
 	nearest_tokens = []
+	non_lexicon = []
 
 	for w in nearest_words:
 		if w[0] in tokens:
-			nearest_tokens.append(w)
+			nearest_tokens.append([w[0], w[1], True])
+		else:
+			non_lexicon.append([w[0], w[1], False])
 
-	nearest_words = nearest_tokens[0:20]
+	nearest_words = nearest_tokens[0:19]
+
+	if len(non_lexicon) == 0 or len(nearest_words) == 0:
+		continue
+
+	if non_lexicon[0][1] > nearest_tokens[0][1]:
+		if editdistance.eval(word, non_lexicon[0][0]) > 2:
+			print("Word: " + word)
+			print("Nearest out of lexicon word: " + non_lexicon[0][0])
+			nearest_words.append(non_lexicon[0])
 
 	number_nearest_words = len(nearest_words)
 
-	similarity_lookup[word] = [{"w": nw[0], "d": round(nw[1], 3)} for nw in nearest_words]
+	similarity_lookup[word] = [{"w": nw[0], "d": round(nw[1], 3), "l": nw[2]} for nw in nearest_words]
 
 print(similarity_lookup)
-dict_2_json(similarity_lookup, "patch615_word2vec_tree.json")
+dict_2_json(similarity_lookup, "fruiting" + "_word2vec_tree.json")
 
 print("Finished!")
