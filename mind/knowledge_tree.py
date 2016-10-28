bcd74d85991bab7c255125f5fdc7a805dc10f78e
Matt Sevrens <msevrens@gmail.com>
2016-10-28 13:55:06 -0700

    cleaning up knowledge tree module

--- mind/knowledge_tree.py
+++ mind/knowledge_tree.py
@@ -36,15 +36,14 @@ def load_ken():
 	df = pd.read_csv("data/input/thought.csv", na_filter=False, encoding="utf-8", error_bad_lines=False)
 	seers_grouped = df.groupby('Seer', as_index=False)
 	seers = dict(list(seers_grouped))
-	thoughts = list(seers["prophet"]["Thought"]) + list(seers["msevrens@yodlee.com"]["Thought"]) + list(seers["msevrens"]["Thought"])
+	thoughts = list(seers[sys.argv[1]]["Thought"])
 	ken = vectorize(thoughts, min_df=1)
 	sorted_ken = sorted(ken.items(), key=operator.itemgetter(1))
 	sorted_ken.reverse()
 
-	keys_in_word2vec = model.vocab.keys()
-	tokens = [x[0] for x in sorted_ken[0:5000] if x[0] in keys_in_word2vec]
+	return sorted_ken
 
-def knowledge_tree():
+def knowledge_tree(ken):
 	"""Build JSON for Tree of Knowledge"""
 	
 	# Load Model
@@ -52,18 +51,8 @@ def knowledge_tree():
 	model = gensim.models.Word2Vec.load('models/prophet_word2vec')  # C binary format
 	print("Loading model: Done")
 
-	# Load Top Words
-	df = pd.read_csv("data/input/thought.csv", na_filter=False, encoding="utf-8", error_bad_lines=False)
-	seers_grouped = df.groupby('Seer', as_index=False)
-	seers = dict(list(seers_grouped))
-	thoughts = list(seers["prophet"]["Thought"]) + list(seers["msevrens@yodlee.com"]["Thought"]) + list(seers["msevrens"]["Thought"])
-	ken = vectorize(thoughts, min_df=1)
-	sorted_ken = sorted(ken.items(), key=operator.itemgetter(1))
-	sorted_ken.reverse()
-
 	keys_in_word2vec = model.vocab.keys()
-	tokens = [x[0] for x in sorted_ken[0:5000] if x[0] in keys_in_word2vec]
-
+	tokens = [x[0] for x in ken[0:5000] if x[0] in keys_in_word2vec]
 	number_words = len(model.vocab)
 	vocab = list(model.vocab.keys())
 
@@ -102,66 +91,10 @@ def knowledge_tree():
 
 	print(similarity_lookup)
 
-	dict_2_json(similarity_lookup, "fruiting" + "_word2vec_tree.json")
+	dict_2_json(similarity_lookup, "fruiting" + "_leah_tree.json")
 
 	return similarity_lookup
 
-def tree_cloud(similarity_lookup, depth=3, seed_word="word2vec"):
-	"""Build CSV for Tree Cloud"""
-
-	weights = defaultdict(list)
-	used_words = [seed_word]
-	max_depth = depth
-	num_children = 5
-
-	if seed_word not in similarity_lookup:
-		print("Seed word not found, please try another word")
-		sys.exit()
-
-	def grow_branch(word, level):
-
-		num_twigs = num_children;
-
-		if level <= 0 or word not in similarity_lookup:
-			return
-
-		if len(similarity_lookup[word]) < num_children:
-			num_twigs = len(similarity_lookup[word])
-
-		for i in range(num_twigs):
-
-			cur_word = similarity_lookup[word][i]["w"]
-
-			if cur_word in used_words:
-				continue
-
-			used_words.append(cur_word)
-			weights[level].append(cur_word)
-
-		for word in weights[level]:
-			new_level = level - 1
-			grow_branch(word, new_level)
-
-	weights[max_depth + 5].append(seed_word)
-	grow_branch(seed_word, max_depth)
-
-	dict_list = []
-
-	for weight, words in weights.items():
-		for word in words:
-			dict_list.append({
-				"weight": weight,
-				"word": word
-			})
-
-	with open('data/output/tree_cloud.tab', 'w') as output_file:
-		dict_writer = csv.DictWriter(output_file, ["weight", "word"], delimiter="\t")
-		dict_writer.writeheader()
-		dict_writer.writerows(dict_list)
-
 if __name__ == "__main__":
-	#ken = load_ken()
-	#similarity_lookup = knowledge_tree(ken)
-
-	similarity_lookup = load_json("data/output/prophet_word2vec_tree.json")
-	tree_cloud(similarity_lookup, depth=10, seed_word="confluesce")
+	ken = load_ken()
+	similarity_lookup = knowledge_tree(ken)
