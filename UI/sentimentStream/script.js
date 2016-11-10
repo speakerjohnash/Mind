a7a67ac4475c7e8656e6c617235543929c0508c0
Matt Sevrens <msevrens@gmail.com>
2016-11-10 12:37:52 -0800

    more tweaks

--- UI/sentimentStream/script.js
+++ UI/sentimentStream/script.js
@@ -32,19 +32,18 @@
 
 		var svg = d3.select(".canvas-frame").append("svg")
 		    .attr("width", width)
-		    .attr("height", height);
+		    .attr("height", height + 20);
+
+		// Data
+		var formattedData = formatData(data)
 
 		// Gradients
-		var polarityRange = d3.extent(data, function(d) { return d["sentiment"] }),
+		var polarityRange = d3.extent(formattedData, function(d) { return d["value"] }),
 			format = d3.time.format("%m/%d/%Y"),
 			timeRange = d3.extent(data, function(d) { return format.parse(d["Post Date"])});
 
-		// Stream
-		var field = svg.append("g")
-    		.attr("class", "field");
-
     	var x = d3.time.scale().domain(timeRange).range([0, width]),
-    		y = d3.scale.linear().domain([-1,1]).range([height, 0]);
+    		y = d3.scale.linear().domain([-1, 1]).range([height, 0]);
 
     	// Line 
     	var line = d3.svg.line()
@@ -55,25 +54,25 @@
 		// Draw Axes
 		var XAxis = d3.svg.axis().scale(x).orient("bottom");
 
-  		field.append("g")
+		var field = svg.append("g").attr("height", height);
+		
+  		var axis = svg.append("g")
 			.attr("class", "x axis")
-			.attr("transform", "translate(0, " + (height - 20) + ")")
+			.attr("transform", "translate(0, " + height + ")")
 			.call(XAxis);
 
-		var formattedData = formatData(data)
+		field.append("path")
+			.datum(formattedData)
+			.attr("class", "line")
+			.attr("d", line);
 
-		svg.append("line")
+		field.append("line")
     		.style("stroke", "black")  // colour the line
 			.attr("x1", 0)     // x position of the first end of the line
 			.attr("y1", height / 2)      // y position of the first end of the line
 			.attr("x2", width)     // x position of the second end of the line
 			.attr("y2", height / 2);    // y position of the second end of the line
 
-		svg.append("g")
-			.append("path")
-			.datum(formattedData)
-			.attr("class", "line")
-			.attr("d", line);
 
 	}
 
