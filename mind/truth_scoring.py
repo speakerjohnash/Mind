b021f92e2c3e4c451d6639a9e2a9cd89ebfc3bd0
Matt Sevrens <msevrens@gmail.com>
2017-01-09 11:35:28 -0800

    adding shell for truth scoring

---
+++ mind/truth_scoring.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python3
+
+"""This module demonstrates the scoring function
+behind Prophet
+
+Created on Jan 09, 2017
+@author: Matthew Sevrens
+"""
+
+#################### USAGE ##########################
+
+# python3 -m mind.truth_scoring
+
+#####################################################
+
+import logging
+import math
+import sys
+
+import numpy as np
+
+logging.basicConfig(level=logging.INFO)
+
+def run_from_command_line():
+	"""Run module from command line"""
+
+if __name__ == "__main__":
+	run_from_command_line()
