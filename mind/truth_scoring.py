6a962a62b08976dfaa29e8a73970b697f0743631
Matt Sevrens <msevrens@gmail.com>
2017-01-14 19:09:19 -0800

    laying some ground work

--- mind/truth_scoring.py
+++ mind/truth_scoring.py
@@ -10,6 +10,7 @@ Created on Jan 09, 2017
 #################### USAGE ##########################
 
 # python3 -m mind.truth_scoring
+# http://www.metaculus.com/help/scoring
 
 #####################################################
 
@@ -19,10 +20,35 @@ import sys
 
 import numpy as np
 
+from mind.tools import load_dict_list
+
 logging.basicConfig(level=logging.INFO)
 
 def run_from_command_line():
 	"""Run module from command line"""
+
+	scores = load_dict_list("data/input/ubs_votingapi_vote.csv")
+	truth_scores = [x for x in scores if x["tag"] == "Prescience"]
+
+	print(len(truth_scores))
+
+	#for score in truth_scores:
+	#	print(score)
+
+	# TODO
+	# Analytics
+	# How many thoughts have more than one truth vote?
+	# How many unique users have logged votes?
+	# How many thoughts have dissonance between users?
+	# What is the distribution of truth votes?
+
+	# Should reward more: correct contrarianism
+	# Should reward less: correct alignment with the crowd
+	# Should penalize more: incorrect contrarianism
+	# Should penalize less: incorrect alignment with the crowd
+
+	# One vote per day
+	# Reflections and Statements logged as predictions of future truth
 
 if __name__ == "__main__":
 	run_from_command_line()
